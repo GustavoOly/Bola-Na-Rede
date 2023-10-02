6278de7a054325c3b5317b9a87394e65ceee8040
Gusttavo Oly <gustavooly.dev@gmail.com>
2023-10-02 15:16:39 -0300

    fim testes, problemas resolvidos

--- src/components/b-whatsapp.jsx
+++ src/components/b-whatsapp.jsx
@@ -1,9 +1,10 @@
+import btnWhatsapp from '/src/assets/whatsapp.svg'
 function BtnWhatsapp(){
 
     return (
         <>
         <div className="whatsapp-button">
-    <img className="b-whatsapp" src="./src/assets/whatsapp.svg" title="Entrar no grupo do whatsapp" alt="botão do whatsapp" />
+    <img className="b-whatsapp" src={btnWhatsapp} title="Entrar no grupo do whatsapp" alt="botão do whatsapp" />
     <div className="continer-whatsapp">Cnncjcijndicndno</div> 
     </div>
     </>
