6ae3c22785ba3d1fb82c2555dba2044e645d81f2
Gusttavo Oly <gustavooly.dev@gmail.com>
2023-10-02 14:34:22 -0300

    teste

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,4 +1,7 @@
+import bolaNaRedeLogo from '/home/gusttavo/trabalhos/bola-na-rede/client/src/assets/Bola-Na-Rede - logo.svg'
+
 function Header() {
+   
    const estiloSection = {
       borderBottom: '1px solid rgba(255, 255, 255, 0.13)',
       backgroundColor: 'black',
@@ -7,7 +10,7 @@ function Header() {
       <section style={estiloSection}>
          <div className="header__container">
             <div className="header__c-logo">
-               <img title="Logo Bola Na Rede" className="header__logo-img" src="./src/assets/Bola-Na-Rede - logo.svg" />
+               <img title="Logo Bola Na Rede" className="header__logo-img" src={bolaNaRedeLogo} />
             </div>
             {/*<nav className="header__nav">
 
