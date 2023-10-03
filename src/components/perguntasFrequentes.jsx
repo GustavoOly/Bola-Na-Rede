a9a23e6387207881b22ad19531d4515daa083198
Gusttavo Oly <gustavooly.dev@gmail.com>
2023-10-03 12:55:02 -0300

    updates FAQ

--- src/components/perguntasFrequentes.jsx
+++ src/components/perguntasFrequentes.jsx
@@ -1,16 +1,33 @@
 const source = [
     {
-        summary: "Quantos Time irão Participar?",
-        details: " ndjndjcnwd wdincwd cdwc jwd cs dcjsdc dcsdjds"
+        summary: "Qual é a localização onde o campeonato de futebol ocorrerá?",
+        details: "Na Fazenda Aliança que está localizada em Ipameri, Goiás. Para obter direções detalhadas, consulte nosso site oficial."
     },
     {
-        summary: "Quantos Time irão Participar?",
-        details: " ndjndjcnwd wdincwd cdwc jwd cs dcjsdc dcsdjds"
+        summary: "Quais são os custos de inscrição para as equipes participantes do campeonato?",
+        details: "As inscrições para equipes são pagas. Entre em contato conosco para obter informações atualizadas sobre as taxas de inscrição."
     },
     {
-        summary: "Quantos Time irão Participar?",
-        details: " ndjndjcnwd wdincwd cdwc jwd cs dcjsdc dcsdjds"
+        summary: "Há alguma taxa de entrada para os espectadores que desejam assistir aos jogos?",
+        details: "A entrada para os espectadores é gratuita. Convidamos todos a se juntarem a nós e torcerem pelas equipes."
     },
+    {
+        summary: "Posso patrocinar o evento e exibir minha marca no site oficial e ao redor do campo?",
+        details: "Sim, aceitamos patrocínios. Entre em contato conosco para discutir as oportunidades de patrocínio e exposição da marca."
+    },
+    {
+        summary: " Como é o campo onde os jogos serão realizados?",
+        details: "O campo é de médio porte e possui gramado bem cuidado. Oferecemos toda a infraestrutura necessária para os jogos, incluindo tenda e bolas."
+    },
+    {
+        summary: "Os uniformes das equipes estão incluídos na inscrição?",
+        details: "Não, os uniformes são de responsabilidade de cada equipe. Cada equipe deve providenciar seus próprios uniformes."
+    },
+    {
+        summary: "Haverá opções de comida e bebida durante o evento?",
+        details: "Sim, teremos opções de comida e bebida disponíveis durante o evento, porém, serão vendidas separadamente."
+    },
+
 
 ]
 
@@ -22,10 +39,10 @@ function PerguntasFrequentes() {
         flexDirection: 'column',
         gap: '20px',
         alignItems: 'center',
-        padding: '4.2rem 10px',
+        padding: '3.5rem 10px',
         backgroundColor: 'rgb(222, 222, 222)',
         color: 'black',
-        borderRadius: '10px 10px 0px 0px',
+        borderRadius: '20px 20px 0px 0px',
         position: 'relative',
         zIdenx: '100'
     }
@@ -35,8 +52,8 @@ function PerguntasFrequentes() {
             <div style={faqContainerStyles} className="faq__container">
                 <h1>FAQs</h1>
                 <article>
-                    {source.map((item) => (
-                        <details key={item.summary}>
+                    {source.map((item, index) => (
+                        <details key={item.summary} open={index===0}>
                             <summary>
                                 {item.summary}
                             </summary>
