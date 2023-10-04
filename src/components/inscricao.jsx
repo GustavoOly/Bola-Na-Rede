0a19e3666ca00da73e9f8006ddbd14c2fd84e0ff
Gusttavo Oly <gustavooly.dev@gmail.com>
2023-10-04 17:43:51 -0300

    testes

--- src/components/inscricao.jsx
+++ src/components/inscricao.jsx
@@ -47,7 +47,7 @@ function Inscricao() {
                         ) : (
                             <form action="https://formsubmit.co/bolanarede.ipa@outlook.com.br" method="POST" onSubmit={handleSubmit} >
                                 <input type="email" placeholder="Digite seu email" title="Digite seu email" name="email" id="inscricao__input-email" required />
-                                <input type="hidden" name="_next" value="https://bolanarede-ipa.netlify.app/thanks" />
+                                <input type="hidden" name="_next" value="http://localhost:5173/" />
                                 <input type="hidden" name="_captcha" value="false" />
                                 <button type='submit' title="Enviar email" disabled={enviando}>
                                     {enviando ? 'Enviando...' : 'Notifique-me'}
