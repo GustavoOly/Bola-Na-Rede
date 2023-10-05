4c0432b2ac752c6c417aaa521e7e1a6f13d04776
Gusttavo Oly <gustavooly.dev@gmail.com>
2023-10-04 22:30:28 -0300

    testes

--- src/components/inscricao.jsx
+++ src/components/inscricao.jsx
@@ -3,19 +3,6 @@ import jogador from '/src/assets/vetor-jogador-bola-na-rede01-min.png';
 import React, { useState } from 'react';
 
 function Inscricao() {
-    const [enviando, setEnviando] = useState(false); // Estado para controlar o texto do botão
-
-
-    const handleEnviarClick = () => {
-        setEnviando(true); // Alterar o estado para "enviando" quando o botão for clicado
-
-        // Aqui você pode adicionar lógica para enviar o email
-
-        // Após o envio, você pode reverter o texto do botão para "Notifique-me"
-        setTimeout(() => {
-            setEnviando(false);
-        }, 2000); // Por exemplo, após 2 segundos (simulando um envio)
-    };
 
     return (
         <section className="inscricao__section">
@@ -33,10 +20,9 @@ function Inscricao() {
                                 <input type="email" placeholder="Digite seu email" title="Digite seu email" name="email" id="inscricao__input-email" required />
                                 <input type="hidden" name="_next" value="https://bolanarede-ipa.netlify.app/" />
                                 <input type="hidden" name="_captcha" value="false" />
-                                <button  type='submit' title="Enviar email" onClick={handleEnviarClick} disabled={enviando}>
-                                    {enviando ? "Enviando..." : "Enviar"}
-                                </button>
+                                <button  type='submit' title="Enviar email" >Notifique-me</button>
                             </form>
+
                     </div>
                     <p>* Alguma dúvida? Verifique nossas <a>perguntas frequentes</a></p>
                 </div>
