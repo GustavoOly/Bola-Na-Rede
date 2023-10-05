1e905e7297d2f6711ec9252d783394ae233ac5a5
Gusttavo Oly <gustavooly.dev@gmail.com>
2023-10-04 22:25:49 -0300

    teste

--- src/components/inscricao.jsx
+++ src/components/inscricao.jsx
@@ -1,39 +1,20 @@
 import jogador from '/src/assets/vetor-jogador-bola-na-rede01-min.png';
-import Confetes from './confetes';
+//import Confetes from './confetes';
 import React, { useState } from 'react';
 
 function Inscricao() {
-    const [emailEnviado, setEmailEnviado] = useState(false);
-    const [enviando, setEnviando] = useState(false);
+    const [enviando, setEnviando] = useState(false); // Estado para controlar o texto do botão
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
 
-        const form = e.target;
-        const formData = new FormData(form);
+    const handleEnviarClick = () => {
+        setEnviando(true); // Alterar o estado para "enviando" quando o botão for clicado
 
-        setEnviando(true);
+        // Aqui você pode adicionar lógica para enviar o email
 
-        const response = await fetch(form.action, {
-            method: 'POST',
-            body: formData,
-        });
-
-        if (response.ok) {
-            setEmailEnviado(true);
-            
-            setTimeout(() => {
-                window.location.href = "https://bolanarede-ipa.netlify.app/";
-            }, 5000);
-        }
-    };
-
-    const styleP = {
-        color: 'black',
-        textAlign: 'center',
-        padding: '15px 0px',
-        backgroundColor: 'white',
-        borderRadius: '50px',
+        // Após o envio, você pode reverter o texto do botão para "Notifique-me"
+        setTimeout(() => {
+            setEnviando(false);
+        }, 2000); // Por exemplo, após 2 segundos (simulando um envio)
     };
 
     return (
@@ -47,21 +28,15 @@ function Inscricao() {
                     </span>
 
                     <div className="inscricao__c-email">
-                        {emailEnviado ? (
-                            <>
-                                <p style={styleP}>Obrigado por enviar seu e-mail!</p>
-                                {Confetes()}
-                            </>
-                        ) : (
-                            <form action="https://formsubmit.co/bolanarede.ipa@outlook.com.br" method="POST" onSubmit={handleSubmit} >
+                        
+                            <form action="https://formsubmit.co/bolanarede.ipa@outlook.com.br" method="POST" >
                                 <input type="email" placeholder="Digite seu email" title="Digite seu email" name="email" id="inscricao__input-email" required />
-                                <input type="hidden" name="_next" value="http://localhost:5173/" />
+                                <input type="hidden" name="_next" value="https://bolanarede-ipa.netlify.app/" />
                                 <input type="hidden" name="_captcha" value="false" />
-                                <button type='submit' title="Enviar email" disabled={enviando}>
-                                    {enviando ? 'Enviando...' : 'Notifique-me'}
+                                <button  type='submit' title="Enviar email" onClick={handleEnviarClick} disabled={enviando}>
+                                    {enviando ? "Enviando..." : "Enviar"}
                                 </button>
                             </form>
-                        )}
                     </div>
                     <p>* Alguma dúvida? Verifique nossas <a>perguntas frequentes</a></p>
                 </div>
